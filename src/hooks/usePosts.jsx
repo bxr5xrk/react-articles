ec38d10dcf38015d282deb56a0fcbd4071510282
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-05-17 19:32:32 +0300

    hook for sort and search

---
+++ src/hooks/usePosts.jsx
@@ -0,0 +1,29 @@
+import { useMemo } from "react";
+
+// sort posts
+export const useSortedPosts = (posts, sort) => {
+    const sortedPosts = useMemo(() => {
+        if (sort) {
+            return [...posts].sort((a, b) => a[sort].localeCompare(b[sort]));
+        } else {
+            return posts;
+        }
+    }, [sort, posts]);
+
+    return sortedPosts;
+};
+
+// search in sorted posts
+export const usePosts = (posts, sort, search) => {
+    const sortedPosts = useSortedPosts(posts, sort);
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(
+            (post) =>
+                post.title.toLowerCase().includes(search) ||
+                post.desc.toLowerCase().includes(search)
+        );
+    }, [search, sortedPosts]);
+
+    return sortedAndSearchedPosts;
+};
