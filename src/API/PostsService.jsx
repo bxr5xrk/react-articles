c51c3febc8f9ff2bede1d366959a60aeb6b32cbd
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-05-30 15:29:11 +0300

    prettier

--- src/API/PostsService.jsx
+++ src/API/PostsService.jsx
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 export default class PostService {
-    static async getPosts(limit = 10, page= 1) {
+    static async getPosts(limit = 10, page = 1) {
         try {
             const response = await axios.get(
-                "https://jsonplaceholder.typicode.com/posts", {
+                "https://jsonplaceholder.typicode.com/posts",
+                {
                     params: {
                         _limit: limit,
                         _page: page,
-                    }
+                    },
                 }
             );
             return response;
